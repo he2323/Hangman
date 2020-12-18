c508054cdc8042d5b112c9f33780b0a5af96f322
he2323 <44843728+he2323@users.noreply.github.com>
2020-12-18 16:29:37 +0100

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,43 @@
+import random
+import time
+
+
+class User:
+    def __init__(self, lives, score, time, nickname):
+        self.lives = lives
+        self.score = score
+        self.constructTime = time
+        self.nickname = nickname
+
+    def userInput(self):
+        return (input(f"Choose letter {self.nickname}: ")).lower()
+
+def getIndexes(word):
+    return [i for i in range(0, len(word)) if userInput == word[i]]
+
+
+words = ("Book", "Handmade", "Bionicle", "Bathroom", "hate", "love", "mindful")
+
+user = User(3, 100, time.time(), "shin")
+# get random word from list
+chosenWord = list((words[random.randint(0, len(words) - 1)]).lower())
+hiddenWord = list("_" * len(chosenWord))
+
+print(chosenWord)
+while "_" in hiddenWord and user.lives > 0 and user.score > 0:
+    userInput = user.userInput()
+    if userInput in chosenWord:
+        indexes = getIndexes(chosenWord)
+
+        for i in indexes:
+            user.score += 20
+            hiddenWord[i] = userInput
+        print("".join(hiddenWord))
+    elif len(userInput) > 0 and userInput not in chosenWord:
+        user.score -= 20
+        user.lives -= 1
+else:
+    if user.score > 0 and user.lives > 0:
+        print(f"user wins with score: {user.score} and {user.lives} lives")
+    else:
+        print(f"{user.nickname} lost")
