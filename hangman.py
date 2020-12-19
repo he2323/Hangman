af2dcb7e9be7938b2b9adcdd942cfb9ec53b16cd
he2323 <44843728+he2323@users.noreply.github.com>
2020-12-20 00:02:28 +0100

    score things
    trying to make some score calculations, but couse of lack of time i haveto pass it for tomorow

--- hangman.py
+++ hangman.py
@@ -2,24 +2,30 @@ import random
 import time
 
 
+def get_indexes(word):
+    return [index for index in range(0, len(word)) if userInput == word[index]]
+
+
 class User:
-    def __init__(self, lives, score, time, nickname):
+    def __init__(self, lives, score, nickname):
         self.lives = lives
         self.score = score
-        self.constructTime = time
+        print(int(time.time()))
+        self.construct_time = time.time()
         self.nickname = nickname
 
     def user_input(self):
         return (input(f"Choose letter {self.nickname}: ")).lower()
+    def time_passed(self, actual_time):
+        print(int(actual_time), int(self.construct_time))
+        return int(actual_time-self.construct_time)
 
 
-def get_indexes(word):
-    return [i for i in range(0, len(word)) if userInput == word[i]]
 
 
 words = ("Book", "Handmade", "Bionicle", "Bathroom", "hate", "love", "mindful")
-
-user = User(3, 100, time.time(), "shin")
+default_points = 10000
+user = User(3, default_points, "shin")
 # get random word from list
 chosenWord = list((words[random.randint(0, len(words) - 1)]).lower())
 hiddenWord = list("_" * len(chosenWord))
@@ -35,10 +41,9 @@ while "_" in hiddenWord and user.lives > 0 and user.score > 0:
             hiddenWord[i] = userInput
         print("".join(hiddenWord))
     elif len(userInput) > 0 and userInput not in chosenWord:
-        user.score -= 20
         user.lives -= 1
 else:
     if user.score > 0 and user.lives > 0:
-        print(f"user wins with score: {user.score} and {user.lives} lives")
+        print(f"user wins with score: {user.score - user.time_passed(time.time())} and {user.lives} lives")
     else:
         print(f"{user.nickname} lost")
